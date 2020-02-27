f44d33d3b63faab4af0636c4dfc8b544a4d93e22
Roberto Alvarez <roberto_utb@hotmail.com>
2020-02-27 02:24:51 -0600

    added login/register/logout frontend

--- frontend/src/components/auth/register.js
+++ frontend/src/components/auth/register.js
@@ -1,10 +1,11 @@
 import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
 
   const [formData, setFormData] = useState({
     name: '',
@@ -21,9 +22,14 @@ const Register = ({ setAlert }) => {
    if(password !== password2) {
     setAlert('Passwords do not match', 'danger');
    } else {
-     console.log('success');
+     register({ name, email, password });
    }
+ };
+
+ if(isAuthenticated){
+   return <Redirect to="/dashboard" />
  }
+
   return (
     <Fragment>
     <h1 className="large text-primary">Sign Up</h1>
@@ -36,7 +42,7 @@ const Register = ({ setAlert }) => {
           name="name"
           value={name}
           onChange={ e => onChange(e)}
-          required
+
         />
       </div>
       <div className="form-group">
@@ -46,7 +52,7 @@ const Register = ({ setAlert }) => {
           name="email"
           value={email}
           onChange={ e => onChange(e)}
-          required
+
         />
         <small className="form-text"
           >This site uses Gravatar so if you want a profile image, use a
@@ -83,7 +89,16 @@ const Register = ({ setAlert }) => {
 };
 
 Register.propTypes = {
-  setAlert: PropTypes.func.isRequired
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
 }
 
-export default connect(null, { setAlert })(Register);
+const mapStateToProps = state =>({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(
+  mapStateToProps,
+  { setAlert, register }
+ )(Register);
